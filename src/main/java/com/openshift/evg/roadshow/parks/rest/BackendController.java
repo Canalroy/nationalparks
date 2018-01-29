9c757c0c9387879fe9fdbab0c8b8c9a17cd97c26
Canalroy <ryota.kanamori@ctc-g.co.jp>
2018-01-29 17:28:21 +0700

    update

--- src/main/java/com/openshift/evg/roadshow/parks/rest/BackendController.java
+++ src/main/java/com/openshift/evg/roadshow/parks/rest/BackendController.java
@@ -18,5 +18,6 @@ public class BackendController{
     @RequestMapping(method = RequestMethod.GET, value = "/", produces = "application/json")
     public Backend get() {
         return new Backend("nationalparks","World Parks", new Coordinates("47.039304", "14.505178"), 4);
+
     }
 }
